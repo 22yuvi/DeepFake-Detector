8057f9c97b6c83ec3e83dee73b83ef81bdf914b9
22yuvi <139131695+22yuvi@users.noreply.github.com>
2024-07-18 17:25:02 +0530

    Update metadata.py

--- metadata.py
+++ metadata.py
@@ -1,11 +1,10 @@
 import json
 import os.path
-from mode import Mode
 
 class MetaData:
 
     def __init__(self, file_name: str, target: str, output_folder: str,
-                 mode: Mode, low: float, high: float,
+                 low: float, high: float,
                  levels: int = 3, amplification: int = 20
                  ):
         self.__data = {
@@ -16,8 +15,6 @@ class MetaData:
             'high': high,
             'levels': levels,
             'amplification': amplification,
-            'mode': mode.name,
-            'date': None,
         }
 
     def __getitem__(self, item):
