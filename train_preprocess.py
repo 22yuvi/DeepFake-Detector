9cad3e9ddbaa67bede8ad7b71f7695122de3ade9
22yuvi <139131695+22yuvi@users.noreply.github.com>
2024-07-20 21:07:09 +0530

    Update train_preprocess.py

--- train_preprocess.py
+++ train_preprocess.py
@@ -11,9 +11,11 @@ from magnify import Magnify
 from metadata import MetaData
 from utilities import *
 
+predictor_name = 'shape_predictor_81_face_landmarks.dat'
+predictor_path = os.path.join(working_dir, predictor_name)
+
 print("Preparing dlib ... ", end='', flush=True)
 detector = dlib.get_frontal_face_detector()
-predictor_path = '/shape_predictor_81_face_landmarks.dat'
 predictor = dlib.shape_predictor(predictor_path)
 print("Done")
 
@@ -26,18 +28,15 @@ print("Done")
 def generate_align_face(data_path, save_path):
     if not os.path.exists(save_path):
         os.makedirs(save_path)
-
     vidlist = os.listdir(data_path)
     vidlist.sort()
     for vidname in vidlist:
         vidpath = data_path + vidname
-
         save_vid_path = meta_dir + vidname[:-4] + '/'
         if os.path.exists(save_vid_path):
             continue
         else:
             os.mkdir(save_vid_path)
-
         try:
             align_face = preprocess_video(vidname[:-4], detector, predictor, mtcnn, vidpath, save_vid_path)
         except:
@@ -46,17 +45,14 @@ def generate_align_face(data_path, save_path):
 def resize_frame(align_path, resize_path):
     if not os.path.exists(resize_path):
         os.makedirs(resize_path)
-
     vidlist = os.listdir(align_path)
     vidlist.sort()
     for vidname in vidlist:
         print("{} - {} ... ".format(align_path, vidname), end='', flush=True)
         vidpath = align_path + vidname + '/'
-
         save_vid_path = resize_path + vidname + '/'
         if not os.path.exists(save_vid_path):
             os.mkdir(save_vid_path)
-
         imglist = os.listdir(vidpath)
         imglist.sort()
         f_h, f_w = None, None
@@ -65,20 +61,16 @@ def resize_frame(align_path, resize_path):
                 continue
             if imgname.endswith('_face.jpg'):
                 continue
-
             imgpath = vidpath + imgname
-
             img = cv2.imread(imgpath)
             if f_h is None:
                 f_h, f_w = img.shape[:2]
                 f_h = ((int(f_h/10)+1)*10)
                 f_w = ((int(f_w/10)+1)*10)
-
             img = cv2.resize(img, (f_w, f_h))
-
             save_img_path = save_vid_path + imgname
             cv2.imwrite(save_img_path, img)
-
+            
         idx = None
         imglist = os.listdir(save_vid_path)
         imglist.sort()
@@ -97,34 +89,34 @@ def resize_frame(align_path, resize_path):
                     new_img = save_vid_path + ("%04d.jpg"%i)
                     os.system("cp " + ori_img + " " + new_img)
             idx = int(imgname[:4]) + 1
+        if idx is not None and idx < 300:
+            for i in range(idx, 300):
+                ori_img = save_vid_path + ("%04d.jpg"%(idx-1))
+                new_img = save_vid_path + ("%04d.jpg"%i)
+                os.system("cp " + ori_img + " " + new_img)
         print("Done")
 
 def generate_align_video(video_frame_path, video_store_path):
     if not os.path.exists(video_store_path):
         os.makedirs(video_store_path)
-
     vidlist = os.listdir(video_frame_path)
     vidlist.sort()
     for vidname in vidlist:
         print("{} - {} ... ".format(video_frame_path, vidname), end='', flush=True)
         vidpath = video_frame_path + vidname +'/'
-
         save_vid_path = video_store_path + vidname + '.avi'
-
         os.system("ffmpeg -i {}%04d.jpg {}".format(vidpath, save_vid_path))
         print("Done")
 
 def generate_mag_video(vid_path, mag_path):
     if not os.path.exists(mag_path):
         os.makedirs(mag_path)
-
     vidlist = os.listdir(vid_path)
     vidlist.sort()
     for vidname in vidlist:
         print("{} - {} ... ".format(video_dir_name, vidname), end='', flush=True)
         vidpath = vid_path + vidname
         save_vid_path = mag_path + vidname
-
         Magnify(MetaData(file_name=vidpath, low=0.833, high=2, levels=1,
                     amplification=10,  output_folder=mag_path, target = save_vid_path)).do_magnify()
         print("Done")
@@ -132,14 +124,11 @@ def generate_mag_video(vid_path, mag_path):
 def generate_mmst_map(mag_path, map_path):
     if not os.path.exists(map_path):
         os.makedirs(map_path)
-
     ROI_h, ROI_w = 5, 5
-
     vidlist = os.listdir(mag_path)
     vidlist.sort()
     for vidname in vidlist:
         print("{} - {} ... ".format(mag_path, vidname), end='', flush=True)
-
         vidpath = mag_path + vidname
         vid = cv2.VideoCapture(vidpath)
         full_st_map = np.zeros((300, 25, 3))
